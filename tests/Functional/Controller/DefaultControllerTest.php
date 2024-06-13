69a1bad9e914257d782f9b8c8c16c9e58b4f1ff3
Anaïs Meynard <99323795+Korakaas@users.noreply.github.com>
2024-06-13 17:04:18 +0200

    11

--- tests/Functional/Controller/DefaultControllerTest.php
+++ tests/Functional/Controller/DefaultControllerTest.php
@@ -6,25 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testHello()
-    {
-        $client = static::createClient();
-        // $crawler = $client->request('GET', '/hello/Bob');
-
-        // $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertResponseIsSuccessful();
-
-        // $this->assertSelectorTextContains('h1', 'Hello, Bob!!');
-    }
-
     public function testIndex()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertResponseIsSuccessful();
 
-        $this->assertSelectorTextContains('ul', 'Panpan');
+        $this->assertSelectorTextContains('h2', 'Les lapins');
+    }
+
+    public function testHello()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/hello/Bob');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('h1', 'Hello, Bob!');
     }
 }
